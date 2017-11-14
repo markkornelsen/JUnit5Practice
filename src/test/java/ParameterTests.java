13e12cd9e59cce45399c1da8123d6ab1a979edd4
Mark Kornelsen <mark.kornelsen@libertymutual.com>
2017-11-14 15:49:21 -0500

    added additional tests

--- src/test/java/ParameterTests.java
+++ src/test/java/ParameterTests.java
@@ -1,22 +1,40 @@
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class ParameterTests {
 
+	@DisplayName("get test parameters from string array")
 	@ParameterizedTest
 	@CsvSource({ "String1", "String2", "String13" })
 	void testWithCsvSource(String arg) {
 		assertThat(arg, containsString("1"));
 	}
 	
+	@DisplayName("get test parameters from getStrings() method")
 	@ParameterizedTest
-	@CsvFileSource(resources = "/resources/testData.csv")
-	void testWithCsvFile(String arg1, String arg2) {
-		
+	@MethodSource("getStreamOfStrings")
+	void testWithStream(String arg) {
+		assertThat(arg, containsString("1"));
 	}
-
+	
+	@DisplayName("get test parameters from CSV file")
+	@ParameterizedTest
+	@CsvFileSource(resources = "/testData.csv")
+	void testWithCsvFile(String arg1, String arg2) {
+		assertThat(arg1, containsString("H"));
+		assertThat(arg2, containsString("H"));
+	}
+	
+	static Stream<String> getStreamOfStrings() {
+		return Stream.of("String1", "String2", "String13");
+	}
 }
